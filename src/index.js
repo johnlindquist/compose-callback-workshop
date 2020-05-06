767d18dc191e54e03ae59ca5000c2c001f525ef6
johnlindquist <johnlindquist@gmail.com>
2020-05-06 13:04:30 -0600

    Wed May  6 13:04:30 MDT 2020

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  <button>Click Me</button>
 </div>
 `;
+
