76e567211dd27ebd88a218b8803a9dc377521f73
johnlindquist <johnlindquist@gmail.com>
2020-05-12 07:42:53 -0600

    Tue May 12 07:42:53 MDT 2020

--- src/index.js
+++ src/index.js
@@ -4,39 +4,63 @@ let button = document.getElementById("button")
 let input = document.getElementById("input")
 
 let buttonClick = listener => {
-  button.addEventListener("click", listener)
+    button.addEventListener("click", listener)
 }
 
 let inputInput = listener => {
-  input.addEventListener("input", listener)
+    input.addEventListener("input", listener)
 }
 
 let interval = listener => {
-  setInterval(listener, 1000)
+    setInterval(listener, 1000)
 }
 
 let timeout = listener => {
-  setTimeout(listener, 1000)
+    setTimeout(listener, 1000)
 }
 
 let promise = async listener => {
-  let result = await new Promise(resolve => {
-    setTimeout(() => {
-      resolve("This was a promise")
-    }, 1000)
-  })
+    let result = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve("This was a promise")
+        }, 1000)
+    })
 
-  listener(result)
+    listener(result)
 }
 
+let getURL = url => async listener => {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    listener(data);
+};
+
 let someValue = listener => {
-  setTimeout(() => {
-    listener({ name: "John" })
-  }, 0)
+    setTimeout(() => {
+        listener({ name: "John" })
+    }, 0)
 }
 
 let someIterable = listener => {
-  setTimeout(() => {
-    [1, 2, 3].forEach(listener)
-  }, 0)
+    setTimeout(() => {
+        [1, 2, 3].forEach(listener)
+    }, 0)
 }
+
+let someGenerator = listener => {
+    let myGenerator = function* () {
+        let i = 0
+
+        while (i < 10) {
+            yield i
+            i++
+        }
+    }
+
+    for (let value of myGenerator()) {
+        listener(value)
+    }
+}
+
+someGenerator(value => console.log(value))
