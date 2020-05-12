c0ce2427e6e22bf33ec0b54867f7f227184d9140
johnlindquist <johnlindquist@gmail.com>
2020-05-12 07:53:03 -0600

    Tue May 12 07:53:03 MDT 2020

--- src/index.js
+++ src/index.js
@@ -6,94 +6,95 @@ let form = document.getElementById("form")
 let mirror = document.getElementById("mirror")
 let list = document.getElementById("list")
 const URL = `https://foamy-closet.glitch.me`;
+//Generate your own server here: https://codesandbox.io/s/github/johnlindquist/swapi-json-server
 
 form.addEventListener("submit", event => {
-  event.preventDefault()
+    event.preventDefault()
 })
 
 
-let buttonClick = destination => {
-  console.log("setup")
-  button.addEventListener("click", destination)
+let buttonClick = listener => {
+    console.log("setup")
+    button.addEventListener("click", listener)
 }
 
-let nameInput = destination => {
-  name.addEventListener("input", destination)
+let nameInput = listener => {
+    name.addEventListener("input", listener)
 }
 
 
-let search = term => async destination => {
-  destination(await fetch(`${URL}/people?name_like=${term}`))
+let search = term => async listener => {
+    listener(await fetch(`${URL}/people?name_like=${term}`))
 }
 
 let logValue = value => {
-  console.log(value)
+    console.log(value)
 }
 
-let withValueFromSecond = (first, second) => destination => {
-  let secondValue = null
-  second(value => {
-    secondValue = value
-  })
+let withValueFromSecond = (first, second) => listener => {
+    let secondValue = null
+    second(value => {
+        secondValue = value
+    })
 
-  first(ignore => {
-    destination(secondValue)
-  })
+    first(ignore => {
+        listener(secondValue)
+    })
 }
 
-let getTargetValue = source => destination => {
-  source(value => {
-    destination(value.target.value)
-  })
+let getTargetValue = broadcaster => listener => {
+    broadcaster(value => {
+        listener(value.target.value)
+    })
 }
 
 
 let clearInput = () => {
-  name.value = ""
+    name.value = ""
 }
 
 let mirrorText = text => {
-  mirror.innerText = text
+    mirror.innerText = text
 }
 
 let share = () => {
-  let destinations = []
+    let destinations = []
 
-  return source => destination => {
-    destinations.push(destination)
-    if (destinations.length > 1) return
-    source(value => {
-      destinations.forEach(fn => fn(value))
-    })
-  }
-}
-
-let switchTo = switcher => source => destination => {
-  source(value => {
-    switcher(value)(destination)
-  })
-}
-
-let map = xf => source => destination => {
-  source(value => {
-    let mapped = xf(value)
-    destination(mapped)
-  })
-}
-
-let filter = pred => source => destination => {
-  source(value => {
-    if (pred(value)) {
-      destination(value)
+    return broadcaster => listener => {
+        destinations.push(listener)
+        if (destinations.length > 1) return
+        broadcaster(value => {
+            destinations.forEach(fn => fn(value))
+        })
     }
-  })
 }
 
-let mapAsync = xf => source => destination => {
-  source(value => {
-    let mapped = xf(value)
-    mapped.then(destination)
-  })
+let switchTo = switcher => broadcaster => listener => {
+    broadcaster(value => {
+        switcher(value)(listener)
+    })
+}
+
+let map = xf => broadcaster => listener => {
+    broadcaster(value => {
+        let mapped = xf(value)
+        listener(mapped)
+    })
+}
+
+let filter = pred => broadcaster => listener => {
+    broadcaster(value => {
+        if (pred(value)) {
+            listener(value)
+        }
+    })
+}
+
+let mapAsync = xf => broadcaster => listener => {
+    broadcaster(value => {
+        let mapped = xf(value)
+        mapped.then(listener)
+    })
 }
 
 
@@ -106,12 +107,13 @@ let getJSON = mapAsync(async response => await response.json())
 let longEnough = filter(text => text.length > 2)
 
 let listResults = value => {
-  list.innerHTML = value.map(item => `<div>${item.name}</div>`).join("")
+    list.innerHTML = value.map(item => `<div>${item.name}</div>`).join("")
 }
 
 let longEnoughName = longEnough(nameInputTargetValue)
+let getNameResults = getJSON(switchTo(name => search(name))(longEnoughName))
 
-getJSON(switchTo(name => search(name))(longEnoughName))(listResults)
+getNameResults(listResults)
 
 let sharedButton = share()(buttonClick)
 
