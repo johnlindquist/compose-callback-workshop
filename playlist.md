6d551577fc19379fc435979eee00c0368493f16a
johnlindquist <johnlindquist@gmail.com>
2020-05-12 06:50:55 -0600

    Tue May 12 06:50:55 MDT 2020

--- playlist.md
+++ playlist.md
@@ -5,3 +5,10 @@
 
 [Edit on Codesandbox](https://codesandbox.io/s/github/johnlindquist/compose-callback-workshop/tree/stop/?module=/src/index.js) 
 
+
+### Count To 
+
+[Go to branch](https://github.com/johnlindquist/compose-callback-workshop/tree/count-to) 
+
+[Edit on Codesandbox](https://codesandbox.io/s/github/johnlindquist/compose-callback-workshop/tree/count-to/?module=/src/index.js) 
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,79 @@
-import "./styles.css"
+import "./styles.css";
 
-import { compose } from "./utils"
-import { createEventBroadcaster, createInterval } from "./broadcasters"
-import { stopWhen, mapTo } from "./operators"
-import { log } from "./listeners"
+let output = document.getElementById("output");
+let writeOutput = value => {
+    output.innerHTML = value;
+};
 
-let stopButton = document.getElementById("stop")
-let stopClick = createEventBroadcaster(stopButton, "click")
+let number = document.getElementById("number");
+let button = document.getElementById("button");
 
-let interval = createInterval(1000)
+let interval = listener => {
+    let id = setInterval(listener, 50);
+    return () => {
+        clearInterval(id);
+    };
+};
 
-let operations = compose(
-    stopWhen(stopClick),
-    mapTo("hi")
-)
+let buttonClick = listener => {
+    button.addEventListener("click", listener);
+};
 
-operations(interval)(log)
+let numberInput = listener => {
+    number.addEventListener("input", listener);
+};
+
+let switchMap = fn => broadcaster => listener => {
+    return broadcaster(value => {
+        return fn(value)(listener);
+    });
+};
+
+let withOtherValue = (firstBroadcaster, secondBroadcaster) => listener => {
+    let secondValue;
+
+    secondBroadcaster(value => (secondValue = value));
+
+    return firstBroadcaster(firstValue => {
+        listener(secondValue);
+    });
+};
+
+let getTargetValue = broadcaster => listener => {
+    return broadcaster(event => {
+        listener(event.target.value);
+    });
+};
+
+let compose = (...fns) =>
+    fns.reduce((prev, next) => (...args) => prev(next(...args)));
+
+let scan = (fn, initial) => broadcaster => listener => {
+    let current = initial;
+
+    return broadcaster(value => {
+        current = fn(current, value);
+
+        listener(current);
+    });
+};
+
+let takeWhile = condition => broadcaster => listener => {
+    let stop = broadcaster(value => {
+        if (!condition(value)) stop();
+        else listener(value);
+    });
+
+    return stop;
+};
+
+let countUpOnInterval = value =>
+    compose(
+        takeWhile(current => current <= value),
+        scan(x => x + 1, 0)
+    )(interval);
+
+compose(
+    switchMap(countUpOnInterval),
+    getTargetValue
+)(withOtherValue(buttonClick, numberInput))(writeOutput);
